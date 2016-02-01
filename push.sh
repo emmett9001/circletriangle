0a549bb8fd48e51425cfed0d53f843f5dbffe270
Emmett Butler <emmett.butler321@gmail.com>
2016-02-01 09:47:09 -0800

    update deploy script

--- push.sh
+++ push.sh
@@ -2,5 +2,5 @@
 
 rm -rf deploy
 hyde gen -c site_prod.yaml
-rsync -Pavz deploy londonoconnor@londonoconnor.com:/home/londonoconnor/public_html/beta
+rsync -Pavz deploy/* londonoconnor@londonoconnor.com:/home/londonoconnor/public_html
 rm -rf deploy
